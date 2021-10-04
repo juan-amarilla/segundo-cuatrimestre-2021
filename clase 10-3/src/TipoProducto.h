183379c60f3073ac30bfca48d0c0d8c15d25688f
juan amarilla <juanamarillasebastian@gmail.com>
2021-10-03 22:13:52 -0300

    documentacion agregada

--- clase 10-3/src/TipoProducto.h
+++ clase 10-3/src/TipoProducto.h
@@ -12,6 +12,13 @@ typedef struct
 
 }eTipoProducto;
 
+/// @fn int verificarTipo(eTipoProducto[], int, int)
+/// @brief verifica que tipo de producto es
+///
+/// @param eTipoProducto unTipo[]
+/// @param int tam
+/// @param int id
+/// @return retorna la posicion si esta bien o -1 si no se encontro
 int verificarTipo(eTipoProducto unTipo[], int tam, int id);
 
 #endif /* TIPOPRODUCTO_H_ */
